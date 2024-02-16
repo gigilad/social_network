2bde97369e5bc9e14e23a8037268c37b5e71339c
gigilad <91905809+gigilad@users.noreply.github.com>
2024-02-16 18:59:59 +0200

    Add files via upload

--- ImagePost.py
+++ ImagePost.py
@@ -1,7 +1,17 @@
 from Post import Post
+# import matplotlib.pyplot as plt
+# from matplotlib.image import imread
 
 
 class ImagePost(Post):
     def __init__(self, post_type, content, owner):
         super().__init__(post_type, content, owner)
+    def __str__(self):
+        return f"{self.owner.get_username()} posted a picture"    
 
+    # def display(self):
+    #     image_data= imread(self.content)
+    #     plt.imshow(image_data)
+    #     plt.axis('off')
+    #     plt.show()
+        
